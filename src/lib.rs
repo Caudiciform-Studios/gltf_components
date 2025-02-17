ef33a10433b2e78944cfad841b927d639bdb739d
Alec Deason <alec@tinycountry.com>
2025-02-16 23:36:20 -0800

    Consider all the kinds of extras

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 use std::collections::HashMap;
 
 use bevy::{
-    prelude::*,
     asset::ron::Deserializer,
+    gltf::{GltfMeshExtras, GltfSceneExtras},
+    prelude::*,
     reflect::serde::TypedReflectDeserializer,
-    gltf::GltfExtras,
 };
 use serde::de::DeserializeSeed as _;
 
@@ -12,28 +12,53 @@ pub fn plugin(app: &mut App) {
     app.add_systems(Update, check_for_gltf_extras);
 }
 
-fn check_for_gltf_extras(
-    world: &mut World,
-) {
+fn check_for_gltf_extras(world: &mut World) {
     let mut reflect_components = vec![];
 
-    for (entity, extras) in world.query_filtered::<(Entity, &GltfExtras), Added<GltfExtras>>().iter(world) {
-        if let Ok(values) = ron::from_str::<HashMap<String, String>>(&extras.value) {
-            for (component_path, component) in values {
-                let type_registry = world.resource::<AppTypeRegistry>().0.read();
-                if let Some(component_type) = type_registry.get_with_type_path(&component_path) {
-                    match Deserializer::from_str(&component) {
-                        Ok(mut deser) =>
-                            match TypedReflectDeserializer::new(component_type, &type_registry)
-                                    .deserialize(&mut deser) {
-                                Ok(reflected) => reflect_components.push((entity, component_path, reflected)),
-                                Err(err) => warn!("{component_path} is invalid: {err}")
+    for (entity, mesh_extras, scene_extras, extras) in world
+        .query_filtered::<(
+            Entity,
+            Option<&GltfMeshExtras>,
+            Option<&GltfSceneExtras>,
+            Option<&GltfExtras>,
+        ), Or<(
+            Added<GltfMeshExtras>,
+            Added<GltfSceneExtras>,
+            Added<GltfExtras>,
+        )>>()
+        .iter(world)
+    {
+        for value in [
+            mesh_extras.map(|e| &e.value),
+            scene_extras.map(|e| &e.value),
+            extras.map(|e| &e.value),
+        ]
+        .into_iter()
+        .filter_map(|v| v)
+        {
+            if let Ok(values) = ron::from_str::<HashMap<String, String>>(value) {
+                for (component_path, component) in values {
+                    let type_registry = world.resource::<AppTypeRegistry>().0.read();
+                    if let Some(component_type) = type_registry.get_with_type_path(&component_path)
+                    {
+                        match Deserializer::from_str(&component) {
+                            Ok(mut deser) => {
+                                match TypedReflectDeserializer::new(component_type, &type_registry)
+                                    .deserialize(&mut deser)
+                                {
+                                    Ok(reflected) => {
+                                        reflect_components.push((entity, component_path, reflected))
+                                    }
+                                    Err(err) => warn!("{component_path} is invalid: {err}"),
+                                }
                             }
-                        Err(err) => warn!("Failed to make deserializer for {component_type:?}: {err}")
+                            Err(err) => {
+                                warn!("Failed to make deserializer for {component_type:?}: {err}")
+                            }
+                        }
+                    } else {
+                        warn!("component not found: {component_path}");
                     }
-
-                } else {
-                    warn!("component not found: {component_path}");
                 }
             }
         }
@@ -46,15 +71,14 @@ fn check_for_gltf_extras(
             if let Ok(mut entity) = world.get_entity_mut(entity) {
                 match type_registry.get_with_type_path(&component_path) {
                     Some(component_registration) => {
-                        match component_registration
-                            .data::<ReflectComponent>() {
-                                Some(reflect_component) => {
-                                    reflect_component.insert(&mut entity, &*reflected, &type_registry)
-                                },
-                                None => warn!("Component `{}` isn't reflectable", component_path)
+                        match component_registration.data::<ReflectComponent>() {
+                            Some(reflect_component) => {
+                                reflect_component.insert(&mut entity, &*reflected, &type_registry)
+                            }
+                            None => warn!("Component `{}` isn't reflectable", component_path),
                         };
-                    },
-                    None => warn!("Unknown component type: `{}`", component_path)
+                    }
+                    None => warn!("Unknown component type: `{}`", component_path),
                 }
             }
         }
